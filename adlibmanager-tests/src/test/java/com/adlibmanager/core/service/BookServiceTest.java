788eadc84d9a2f4ee13ba60f266d6be19cc09a33
Syed Ali Dilawar Shah <Syedalidilawarshah@gmail.com>
2025-06-28 06:07:36 +0200

    Add and test BookService

---
+++ adlibmanager-tests/src/test/java/com/adlibmanager/core/service/BookServiceTest.java
@@ -0,0 +1,39 @@
+package com.adlibmanager.core.service;
+
+import com.adlibmanager.core.domain.Book;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class BookServiceTest {
+
+    private BookService bookService;
+
+    @Before
+    public void setUp() {
+        bookService = new BookServiceImpl(); // direct implementation for now
+    }
+
+    @Test
+    public void shouldAddBookAndFindIt() {
+        Book book = new Book("1", "Java 8 Basics", "John Doe", "ISBN123", true);
+        bookService.addBook(book);
+
+        List<Book> results = bookService.searchBooks("Java");
+        assertEquals(1, results.size());
+        assertEquals("Java 8 Basics", results.get(0).getTitle());
+    }
+
+    @Test
+    public void shouldDeleteBookById() {
+        Book book = new Book("2", "Clean Code", "Uncle Bob", "ISBN456", true);
+        bookService.addBook(book);
+        bookService.deleteBook("2");
+
+        List<Book> results = bookService.searchBooks("Clean");
+        assertEquals(0, results.size());
+    }
+}
